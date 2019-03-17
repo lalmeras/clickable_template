7f7559fab91527f5b6e4833d35e830d5039b06a0
Laurent Almeras <lalmeras@gmail.com>
2019-03-17 10:32:09 +0100

    improvement of bootstrap.sh

--- {{cookiecutter.project_name}}/bootstrap/bootstrap.sh
+++ {{cookiecutter.project_name}}/bootstrap/bootstrap.sh
@@ -5,6 +5,6 @@ THIS_PATH="$( dirname "$THIS_SCRIPT" )"
 : ${BOOTSTRAP_PY_BRANCH:=master}
 : ${BOOTSTRAP_PY:=https://raw.githubusercontent.com/lalmeras/clickable_bootstrap/master/bootstrap.py}
 
-BOOTSTRAP_COMMAND="poetry config settings.virtualenvs.create false && cd bootstrap && poetry install -vvv"
+BOOTSTRAP_COMMAND="poetry config settings.virtualenvs.create false && cd '$THIS_PATH' && poetry install -v"
 
 wget -q -O - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="$BOOTSTRAP_COMMAND" BOOTSTRAP_PATH="$THIS_PATH" python - "$@"
